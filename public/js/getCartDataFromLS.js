0ed0a5e1caad58ff4d9248aeaa7a071fdebed0ad
Sayantan Dutta <sayantan.dutta@codeclouds.in>
2025-05-29 03:39:18 +0530

    Remove CSS JS Fonts fromm public

--- public/js/getCartDataFromLS.js
+++
@@ -1,9 +0,0 @@
-export const getCartDataFromLS = () => {
-    let cartProducts = localStorage.getItem("cartProductsLS");
-    if (!cartProducts) {
-        return [];
-    }
-
-    cartProducts = JSON.parse(cartProducts);
-    return cartProducts;
-}
